9c8b0c3e69e77522fb44c41f216b8e81ef511c96
Skabed <gmadorell@gmail.com>
2014-12-08 18:23:09 +0100

    BlogPost model now uses datetime instead of datefield.

--- djagolb/blog/migrations/0001_initial.py
+++ djagolb/blog/migrations/0001_initial.py
@@ -13,11 +13,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='BlogPostModel',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('posted_at', models.DateField(auto_created=True)),
-                ('edited_at', models.DateField(auto_now=True)),
-                ('slug', models.SlugField()),
-                ('content', models.TextField()),
+                ('posted_at', models.DateTimeField(auto_now_add=True)),
+                ('edited_at', models.DateTimeField(auto_now=True)),
+                ('slug', models.SlugField(serialize=False, primary_key=True)),
+                ('md_content', models.TextField()),
+                ('html_content', models.TextField(default=b'', editable=False)),
                 ('title', models.CharField(max_length=240)),
             ],
             options={
--- djagolb/blog/models.py
+++ djagolb/blog/models.py
@@ -3,8 +3,8 @@ import pypandoc
 
 
 class BlogPostModel(models.Model):
-    posted_at = models.DateField(auto_now_add=True, auto_now=False)
-    edited_at = models.DateField(auto_now=True)
+    posted_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    edited_at = models.DateTimeField(auto_now=True)
 
     slug = models.SlugField(primary_key=True)
     md_content = models.TextField()
@@ -12,10 +12,8 @@ class BlogPostModel(models.Model):
     title = models.CharField(max_length=240)
 
     def save(self, *args, **kwargs):
-        markdown = self.html_content
-        html = pypandoc.convert(markdown, "html", "md")
-        
-        self.html_content = html
+        markdown = self.md_content
+        self.html_content = pypandoc.convert(markdown, "html", "md")
         super(BlogPostModel, self).save(args, kwargs)
 
     def __unicode__(self):
