e431c4cde7670172ea3afc338a3ad996dc7b583f
Skabed <gmadorell@gmail.com>
2014-12-08 17:52:44 +0100

    Added bootstrap files as static to use for offline development.

--- djagolb/blog/views.py
+++ djagolb/blog/views.py
@@ -7,6 +7,9 @@ class BlogIndexView(generic.ListView):
     template_name = "blog/blog_index.html"
     model = BlogPostModel
 
+    def get_queryset(self):
+        return self.model.objects.order_by("-posted_at")
+
 
 class BlogPostDetail(generic.DetailView):
     template_name = "blog/blogpost.html"
